ba018d93b3c028fd898fc899f446d43c27b1dc0e
Don't want to use my name <rwwmr098@gmail.com>
2021-09-24 23:48:46 +0400

    done

---
+++ app.js
@@ -0,0 +1,38 @@
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+let slideIndex = 0;
+
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        slideIndex++
+        if (slideIndex === slidesCount) {
+            slideIndex = 0
+        }
+    } else if (direction === 'down') {
+        slideIndex--
+        if (slideIndex < 0) {
+            console.log(slideIndex);
+            slideIndex = slidesCount - 1
+            console.log(slideIndex)
+        }
+    }
+    const height = container.clientHeight;
+    mainSlide.style.transform = `translateY(-${slideIndex * height}px)`
+    sidebar.style.transform = `translateY(${slideIndex * height}px)`
+};
